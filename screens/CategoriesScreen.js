710713ab24b1cfbab7b2eb8a597af09c8f425006
JeremyLleva <llevajeremy@yahoo.com>
2020-07-27 10:52:16 -0700

    boilerplate code

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,22 @@
+/** @format */
+
+import React from 'react'
+import {} from 'react-native'
+
+const CategoriesScreen = (props) => {
+    return (
+        <View style={styles.screen}>
+            <Text>The Categories Screen!</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+})
+
+export default CategoriesScreen
